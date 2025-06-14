58747258c5c4ae1ffd4ae98be82c0999342ef07f
data-gitpg <priyankagarg2708@gmail.com>
2025-06-13 22:40:06 -0500

    Add files via upload

--- For loop.py
+++ For loop.py
@@ -68,3 +68,11 @@ for i in range(1,11):
     print(''* (n-i) + '*' *(i))
 
 
+#Another way
+num= int(input("Enter the number of rows ::"))
+for i in range(0,num):
+    for j in range (0,num-i-1):
+        print(end="")
+    for j in range(0,i+1):
+        print("*",end="")
+    print()
